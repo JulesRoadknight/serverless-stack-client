05d5cad5dd01f1242d6b207b211184e2eb68cc8c
Jimjule <roadknightj@gmail.com>
2020-01-13 17:12:37 +0000

    Billing fix

--- src/config.js
+++ src/config.js
@@ -38,12 +38,13 @@
   const prod = {
     STRIPE_KEY: "pk_test_W677BZx5Uye0LeFn8XTeVwgL00uFK3N3y0",
     s3: {
-      REGION: "us",
+      REGION: "us-east-2",
       BUCKET: "jules-practise-bucket"
     },
     apiGateway: {
       REGION: "us-east-2",
-      URL: "https://zv82kfjxp7.execute-api.us-east-2.amazonaws.com/prod"
+      // URL: "https://zv82kfjxp7.execute-api.us-east-2.amazonaws.com/prod"
+      URL: "https://6q7ssfp9zd.execute-api.us-east-2.amazonaws.com/prod"
     },
     cognito: {
       REGION: "us-east-2",
@@ -51,6 +52,12 @@
       APP_CLIENT_ID: "1uai97b4m2ee5mnq54rolptq56",
       IDENTITY_POOL_ID: "us-east-2:576b1aa2-afc5-4e2a-b453-11372783d818"
     }
+    // cognito: {
+    //   REGION: "us-east-2",
+    //   USER_POOL_ID: "us-east-2_S5Ar77Wh5",
+    //   APP_CLIENT_ID: "5r4baemmprg7fti3l3nmisdugo",
+    //   IDENTITY_POOL_ID: "us-east-2:3fcbff43-4b61-4995-b7f9-6a851d89c9db"
+    // }
   };
   
   // Default to dev if not set
